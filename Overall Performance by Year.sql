4eb1d8c472cb9330134934a62c9c375f9f342920
Sylvia Princesa <83403768+sylviaprincesaa@users.noreply.github.com>
2022-02-22 14:45:17 +0700

    Delete Overall Performance by Year.sql

--- Overall Performance by Year.sql
+++
@@ -1,4 +0,0 @@
-SELECT year(order_date) AS "years", SUM(sales) AS "sales", COUNT(order_status) AS "number_of_order"
-FROM dqlab_sales_store
-WHERE order_status = "order finished"
-GROUP BY year(order_date)
